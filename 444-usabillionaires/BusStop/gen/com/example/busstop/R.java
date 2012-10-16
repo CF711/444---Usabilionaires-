789d04c0c573ebf6ea7a25f2146def69b7b8d26b
Alex Kelley <ajk6045@rit.edu>
2012-10-16 01:33:15 -0400

    Pretty much done with fav/hist VIEW(no backend yet)
    the button on the bottom of the last view asking if the user wants to
    use the selected trip is covered up by the txt of the info

--- 444-usabillionaires/BusStop/gen/com/example/busstop/R.java
+++ 444-usabillionaires/BusStop/gen/com/example/busstop/R.java
@@ -13,16 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ic_tab_fav_selected=0x7f020002;
+        public static final int ic_tab_fav_unselected=0x7f020003;
+        public static final int ic_tab_hist_selected=0x7f020004;
+        public static final int ic_tab_hist_unselected=0x7f020005;
+        public static final int icon_favs_tab=0x7f020006;
+        public static final int icon_hist_tab=0x7f020007;
     }
     public static final class id {
-        public static final int Browse=0x7f070005;
-        public static final int FavHist=0x7f070006;
-        public static final int Favorites=0x7f070001;
-        public static final int History=0x7f070002;
+        public static final int Browse=0x7f070003;
+        public static final int FavHist=0x7f070004;
         public static final int LinearLayout=0x7f070000;
-        public static final int ListView=0x7f070003;
-        public static final int MakeTrip=0x7f070004;
-        public static final int menu_settings=0x7f070007;
+        public static final int LinearLayout01=0x7f070001;
+        public static final int LinearLayout1=0x7f070005;
+        public static final int MakeTrip=0x7f070002;
+        public static final int menu_settings=0x7f070006;
     }
     public static final class layout {
         public static final int activity_browse=0x7f030000;
@@ -31,6 +36,7 @@ public final class R {
         public static final int activity_history=0x7f030003;
         public static final int activity_main=0x7f030004;
         public static final int activity_plan_trip=0x7f030005;
+        public static final int activity_reuse_trip=0x7f030006;
     }
     public static final class menu {
         public static final int activity_browse=0x7f060000;
@@ -39,6 +45,7 @@ public final class R {
         public static final int activity_history=0x7f060003;
         public static final int activity_main=0x7f060004;
         public static final int activity_plan_trip=0x7f060005;
+        public static final int activity_reuse_trip=0x7f060006;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -47,16 +54,24 @@ public final class R {
         public static final int button_favorites=0x7f040004;
         public static final int button_history=0x7f040005;
         public static final int button_plan_trip=0x7f040001;
-        public static final int hello_world=0x7f04000a;
-        public static final int menu_settings=0x7f040008;
-        public static final int text_browse=0x7f040007;
-        public static final int text_plan_trip=0x7f040006;
-        public static final int title_activity_browse=0x7f04000c;
-        public static final int title_activity_fav_history=0x7f04000e;
-        public static final int title_activity_favorites=0x7f04000f;
-        public static final int title_activity_history=0x7f04000d;
-        public static final int title_activity_main=0x7f040009;
-        public static final int title_activity_plan_trip=0x7f04000b;
+        public static final int button_return=0x7f04000c;
+        public static final int button_testfav=0x7f040006;
+        public static final int button_testhist01=0x7f040007;
+        public static final int button_testhist02=0x7f040008;
+        public static final int button_use_trip=0x7f040009;
+        public static final int favs_tab=0x7f04000a;
+        public static final int hello_world=0x7f040011;
+        public static final int hist_tab=0x7f04000b;
+        public static final int menu_settings=0x7f04000f;
+        public static final int text_browse=0x7f04000e;
+        public static final int text_plan_trip=0x7f04000d;
+        public static final int title_activity_browse=0x7f040013;
+        public static final int title_activity_fav_history=0x7f040015;
+        public static final int title_activity_favorites=0x7f040016;
+        public static final int title_activity_history=0x7f040014;
+        public static final int title_activity_main=0x7f040010;
+        public static final int title_activity_plan_trip=0x7f040012;
+        public static final int title_activity_reuse_trip=0x7f040017;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- 444-usabillionaires/BusStop/src/com/example/busstop/FavHistoryActivity.java
+++ 444-usabillionaires/BusStop/src/com/example/busstop/FavHistoryActivity.java
@@ -1,18 +1,38 @@
 package com.example.busstop;
 
-import android.os.Bundle;
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class FavHistoryActivity extends Activity {
-
+public class FavHistoryActivity extends TabActivity {
+	
+	TabHost tabHost;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fav_history);
+        
+        TabHost tabHost = getTabHost();
+        
+        TabSpec favSpec = tabHost.newTabSpec("Favorites");
+        favSpec.setIndicator("Favorites", getResources().getDrawable(R.drawable.icon_favs_tab));
+        Intent favintent = new Intent(this, FavoritesActivity.class);
+        favSpec.setContent(favintent);
+        
+        TabSpec histSpec = tabHost.newTabSpec("History");
+        histSpec.setIndicator("History", getResources().getDrawable(R.drawable.icon_hist_tab));
+        Intent histintent = new Intent(this, HistoryActivity.class);
+        histSpec.setContent(histintent);
+        
+        tabHost.addTab(favSpec);
+        tabHost.addTab(histSpec);
+        
     }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
