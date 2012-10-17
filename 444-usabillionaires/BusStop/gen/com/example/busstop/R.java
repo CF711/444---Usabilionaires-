d367c4013fe35f5a8a0703f5feb1ac2adf1f5505
Chris F <cxf1897@rit.edu>
2012-10-17 14:35:53 -0400

    Testing commit again

--- 444-usabillionaires/BusStop/gen/com/example/busstop/R.java
+++ 444-usabillionaires/BusStop/gen/com/example/busstop/R.java
@@ -82,6 +82,7 @@ public final class R {
         public static final int hello_world=0x7f040016;
         public static final int hist_tab=0x7f04000c;
         public static final int menu_settings=0x7f040014;
+        public static final int test=0x7f04001d;
         public static final int text_another_dest=0x7f04000f;
         public static final int text_browse=0x7f040013;
         public static final int text_finish_trip=0x7f040010;
