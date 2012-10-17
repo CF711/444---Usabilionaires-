3c0703351e51666c761346dbee27a04f2a5d8a13
Chris F <cxf1897@rit.edu>
2012-10-17 19:30:46 -0400

    Updated BusStop class

--- 444-usabillionaires/BusStop/src/com/example/busstop/BusStop.java
+++ 444-usabillionaires/BusStop/src/com/example/busstop/BusStop.java
@@ -1,5 +1,25 @@
 package com.example.busstop;
 
 public class BusStop {
-
+	public String name;
+	public String startTime;
+	public String endTime;
+	
+	public BusStop(String name, String startTime, String endTime){
+		this.name = name;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getStartTime(){
+		return this.startTime;
+	}
+	
+	public String getEndTime(){
+		return this.endTime;
+	}
 }
