78e6113b159d4c41e3810fd63cff5fdd011c338b
Alex Kelley <ajk6045@rit.edu>
2012-10-17 21:08:06 -0400

    added zacks changed view

--- 444-usabillionaires/BusStop/src/com/example/busstop/PlanTripActivity.java
+++ 444-usabillionaires/BusStop/src/com/example/busstop/PlanTripActivity.java
@@ -1,9 +1,12 @@
 package com.example.busstop;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class PlanTripActivity extends Activity {
@@ -13,24 +16,31 @@ public class PlanTripActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_trip);
         
-        ListView listView = (ListView) findViewById(R.id.listView1);
-        String[] values = new String[] { "Colony, 2:42PM 2:45PM", "Colony, 4:42PM 4:45PM", "perkins, 3:42PM 3:45PM", "Park Point S, 2:42PM 2:45PM",
-          "Gleason Circle, 11:42AM 11:45AM", "Gleason Circle, 2:42PM 2:45PM" };
+        //ListView listView = (ListView) findViewById(R.id.listView1);
+        //String[] values = new String[] { "Colony, 2:42PM 2:45PM", "Colony, 4:42PM 4:45PM", "perkins, 3:42PM 3:45PM", "Park Point S, 2:42PM 2:45PM",
+          //"Gleason Circle, 11:42AM 11:45AM", "Gleason Circle, 2:42PM 2:45PM" };
 
         // First paramenter - Context
         // Second parameter - Layout for the row
         // Third parameter - ID of the TextView to which the data is written
         // Forth - the Array of data
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-          android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+          //android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
         // Assign adapter to ListView
-        listView.setAdapter(adapter);
+        //listView.setAdapter(adapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_plan_trip, menu);
         return true;
+    }
+    
+    public void selectDestination(View view){
+    }
+    
+    public void finishTrip(View view){
+
     }
 }
